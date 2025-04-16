df9f6df13bc6e01f51289a824f825f0cd48b3625
Amanda Dantas <78170677+amandadantasds@users.noreply.github.com>
2025-04-15 21:09:39 -0300

    Update ex10.component.ts
    Alterando componente ex10

--- src/app/components/ex10/ex10.component.ts
+++ src/app/components/ex10/ex10.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ex10',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './ex10.component.html',
   styleUrl: './ex10.component.css'
 })
 export class Ex10Component {
-
+  valor1: number = 0;
+  valor2: number = 0;
+  operacao: string = '';
 }
